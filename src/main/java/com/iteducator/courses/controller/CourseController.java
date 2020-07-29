d0a5d99cf6b82f088ab3f9a7be2233fc28d4dcd9
Vladyslav Krys <vladyslav.krys@gmail.com>
2020-07-29 15:58:20 +0300

    Add CourseService with basic commands

--- src/main/java/com/iteducator/courses/controller/CourseController.java
+++ src/main/java/com/iteducator/courses/controller/CourseController.java
@@ -1,31 +1,54 @@
 package com.iteducator.courses.controller;
 
 import com.iteducator.courses.model.Course;
-import com.iteducator.courses.repository.CourseRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.iteducator.courses.service.CourseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/courses")
 public class CourseController {
 
-    @Autowired
-    private CourseRepository courseRepository;
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
 
     @GetMapping("/all")
     public List<Course> getCourses() {
-        return courseRepository.findAll();
+        return courseService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getCourse(@PathVariable String id) {
+        Course course = courseService.findById(id);
+
+        return new ResponseEntity<>(course, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCourse(@PathVariable String id) {
+        courseService.deleteById(id);
+
+        return new ResponseEntity<>("Course with ID: ".concat(id).concat(" was deleted"), HttpStatus.OK);
+    }
+
+    @PostMapping("/{course}")
+    public ResponseEntity<?> createCourse(@RequestBody Course course) {
+        Course createdCourse = courseService.saveOrUpdateCourse(course);
+
+        return new ResponseEntity<>(createdCourse, HttpStatus.CREATED);
     }
 
     @PostMapping("/add-random")
-    public Course addRandomCourse(){
+    public Course addRandomCourse() {
         Course course = new Course(UUID.randomUUID().toString(), "First course");
-        return courseRepository.save(course);
+        return courseService.saveOrUpdateCourse(course);
     }
 }
